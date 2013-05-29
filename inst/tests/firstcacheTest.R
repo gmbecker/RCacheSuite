7fa6be9b97f3eab21368d4a5ed8df8a623b8552f
gmbecker <gmbecker@ucdavis.edu>
2013-05-29 14:34:03 -0700

    another small update to inst/tests/firstcacheTest.R

--- inst/tests/firstcacheTest.R
+++ inst/tests/firstcacheTest.R
@@ -3,8 +3,8 @@ library(RCacheSuite)
 x = 5
 
 mycache = cacheClass$new(type="both", cache_dirs= "./")
-
 print(mycache)
+
 evalWithCache("y=x+5", cache = mycache, verbose=TRUE)
 
 #same as above
@@ -19,8 +19,12 @@ evalWithCache("y=x+5", cache = mycache, verbose=TRUE)
 evalWithCache("y = x+6", cache = mycache, verbose=TRUE)
 #same as above
 evalWithCache("y     =    x + 6", cache = mycache, verbose=TRUE)
+#write the in-memory cached data to disk
+mycache$to_disk()
 x = 7
 #different due to new value of x
 evalWithCache("y = x + 6", verbose=TRUE, cache=mycache)
 
 print(mycache)
+
+#this last one didn't get written to disk, so when you quit R, start it again, and rerun the script, there won't be a cache for it, but there will for the rest of them.
