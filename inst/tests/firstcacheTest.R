e9e52acac2a641c39c9463d78d5f4da739d7f8cb
gmbecker <gmbecker@ucdavis.edu>
2013-05-29 14:21:35 -0700

    small update to inst/tests/firstcacheTest.R

--- inst/tests/firstcacheTest.R
+++ inst/tests/firstcacheTest.R
@@ -1,22 +1,26 @@
 
 library(RCacheSuite)
 x = 5
-#evalWithCache("y=x+5", verbose=TRUE)
-
-#evalWithCache("y=x+5", verbose=TRUE)
-
-#evalWithCache("y = \nx+    5", verbose=TRUE)
-
-x=6
-evalWithCache("y=x+5", verbose=TRUE)
 
 mycache = cacheClass$new(type="both", cache_dirs= "./")
 
+print(mycache)
 evalWithCache("y=x+5", cache = mycache, verbose=TRUE)
 
-evalWithCache("y = x+6", cache = mycache, verbose=TRUE)
+#same as above
+evalWithCache("y =          x+5", cache = mycache, verbose=TRUE)
 
+print(mycache)
+x = 6
+#different than above because of new value of x
+evalWithCache("y=x+5", cache = mycache, verbose=TRUE)
+
+#different tvhan all previous due to new code
+evalWithCache("y = x+6", cache = mycache, verbose=TRUE)
+#same as above
 evalWithCache("y     =    x + 6", cache = mycache, verbose=TRUE)
 x = 7
+#different due to new value of x
 evalWithCache("y = x + 6", verbose=TRUE, cache=mycache)
 
+print(mycache)
